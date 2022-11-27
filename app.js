6f77cd4c579ba590373ba6ec8cb827fd0e11df7d
Nischay <goyalivuq1@gmail.com>
2022-11-28 01:21:02 +0530

    Done with changes

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ const https=require("https");
 const { appendFile } = require("fs");
 const { json } = require("express");
 const { request } = require("http");
+require("dotenv").config();
 
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'))
 
 app.get("/",function(req,res){
-    res.sendFile(__dirname+"/signup.html")    
+    res.sendFile(__dirname+"/public/signup.html")    
 })
-
+app.post("/f",function(req,res){
+    res.redirect("/");
+})
 app.post("/", function(req,res){
  let fname=req.body.name;
  let lname=req.body.lname;
@@ -30,20 +33,22 @@ app.post("/", function(req,res){
         }
     ]
  }
+ const apikey=process.env.APIKEY;
+ app.post("/")
  const options={
     method:"POST",
-    auth:"nischay:566a534747c501436bfafbe2367505aa-us10"
+    auth:"nischay:"+apikey
  }
  const jsondata=JSON.stringify(data);
 const url="https://us10.api.mailchimp.com/3.0/lists/54013afcf4"
  const request=https.request(url,options,function(response){
         let sc=response.statusCode;
-        console.log(typeof(sc));
+        console.log(sc);
         if(sc===200)
         {
-            res.sendFile(__dirname+"/suc.html")
+            res.sendFile(__dirname+"/public/suc.html")
         }
-        else res.sendFile(__dirname+"/failure.html");
+        else res.sendFile(__dirname+"/public/failure.html");
  })
  request.write(jsondata);
  request.end();
